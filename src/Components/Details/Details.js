de9e26027c32240883db5b2fd205d3b06532e15b
Gogo-58BG <gogo_58bg@yahoo.com>
2021-03-22 17:36:10 +0200

    Details component added

---
+++ src/Components/Details/Details.js
@@ -0,0 +1,19 @@
+const Details = () => {
+    return (
+        <div class="container details">
+        <div class="details-content">
+            <h2>Title</h2>
+             <img class="detail-img" src="" alt="traxxas-image" />
+            <strong>Category</strong>
+            <p>Description</p>
+            <div class="buttons">
+                <a href="#" class="btn delete">Delete</a>
+                <a href="#" class="btn edit">Edit</a>
+                <a href="#" class="btn edit">Back</a>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default Details;
